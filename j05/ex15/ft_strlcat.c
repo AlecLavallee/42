f5b342229ca59fdc813e679761e7c76965761180
alec lavallée <alec.lavallee89@gmail.com>
2018-10-23 16:40:27 +0200

    test ft_strlcat.c

--- j05/ex15/ft_strlcat.c
+++ j05/ex15/ft_strlcat.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <string.h>
 
 int	main(void)
 {
-
+	char str1[50] = "je suis le test";
+	char str2[50] = "test";
+	printf("%d", strlcat(str1, str2));
+}
